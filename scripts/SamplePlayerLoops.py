f35da00e3f07269a94af7b55301486f5d0b20d97
Shawn Pinciara <shawnpinciara@gmail.com>
2023-09-21 22:16:57 +0200

    Updates

--- scripts/SamplePlayerLoops.py
+++ scripts/SamplePlayerLoops.py
@@ -1,11 +1,7 @@
-# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
-
-"""CircuitPython Essentials Audio Out WAV example"""
-import time, board,digitalio,audiomixer,analogio
+import time, board, digitalio, audiomixer, analogio
 from audiocore import WaveFile
 from adafruit_ticks import ticks_ms, ticks_add, ticks_less, ticks_diff
+import asyncio
 
 
 try:
@@ -15,12 +11,13 @@ except ImportError:
         from audiopwmio import PWMAudioOut as AudioOut
     except ImportError:
         pass  # not always supported by every board!
-#VARIABLES
-bpm = 300
-bpm_millis = int((60*1000)/bpm)
+# VARIABLES
+bpm = 400
+bpm_millis = int((60 * 1000) / bpm)
+bpm_float = 60 / bpm
 
 
-#INPUTS
+# INPUTS
 btn1 = digitalio.DigitalInOut(board.GP27)
 btn1.switch_to_input(pull=digitalio.Pull.UP)
 led1 = digitalio.DigitalInOut(board.GP26)
@@ -37,14 +34,33 @@ btn4 = digitalio.DigitalInOut(board.GP11)
 btn4.switch_to_input(pull=digitalio.Pull.UP)
 led4 = digitalio.DigitalInOut(board.GP10)
 led4.direction = digitalio.Direction.OUTPUT
-pot1 = analogio.AnalogIn(board.GP28_A2) #to read it: pot1.value 0 to 65535
-#pot2 = analogio.AnalogIn(board.GP26_A0)
+btn5 = digitalio.DigitalInOut(board.GP9)
+btn5.switch_to_input(pull=digitalio.Pull.UP)
+led5 = digitalio.DigitalInOut(board.GP8)
+led5.direction = digitalio.Direction.OUTPUT
+btn6 = digitalio.DigitalInOut(board.GP7)
+btn6.switch_to_input(pull=digitalio.Pull.UP)
+led6 = digitalio.DigitalInOut(board.GP6)
+led6.direction = digitalio.Direction.OUTPUT
+btn7 = digitalio.DigitalInOut(board.GP5)
+btn7.switch_to_input(pull=digitalio.Pull.UP)
+led7 = digitalio.DigitalInOut(board.GP4)
+led7.direction = digitalio.Direction.OUTPUT
+btn8 = digitalio.DigitalInOut(board.GP3)
+btn8.switch_to_input(pull=digitalio.Pull.UP)
+led8 = digitalio.DigitalInOut(board.GP2)
+led8.direction = digitalio.Direction.OUTPUT
 
-btns = [btn1,btn2,btn3,btn4]
-leds = [led1,led2,led3,led4]
+current_btn_value = 0
+
+pot1 = analogio.AnalogIn(board.GP28_A2)  # to read it: pot1.value 0 to 65535
+# pot2 = analogio.AnalogIn(board.GP26_A0)
+
+btns = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8]
+leds = [led1, led2, led3, led4, led5, led6, led7, led8]
 
 
-#SOUNDS:
+# SOUNDS:
 loop = WaveFile(open("/loops/1/loop.wav", "rb"))
 loop1 = WaveFile(open("/loops/1/loop1.wav", "rb"))
 loop2 = WaveFile(open("/loops/1/loop2.wav", "rb"))
@@ -54,38 +70,51 @@ kick = WaveFile(open("/loops/todbot/kick.wav", "rb"))
 hihat = WaveFile(open("/loops/todbot/hihat.wav", "rb"))
 snare = WaveFile(open("/loops/todbot/snare.wav", "rb"))
 
-#MIXER (OUTS)
-mixer = audiomixer.Mixer(voice_count=4, sample_rate=22050, channel_count=1,bits_per_sample=16, samples_signed=True)
+# MIXER (OUTS)
+mixer = audiomixer.Mixer(
+    voice_count=4,
+    sample_rate=22050,
+    channel_count=1,
+    bits_per_sample=16,
+    samples_signed=True,
+)
 audio = AudioOut(board.GP0)
 audio.play(mixer)
 
-#PROGRAM VARIABLES
-sequence1 = [1,0,0,0] #kick
-sequence2 = [0,0,1,0] #snare
-sequence3 = [1,1,0,1] #hihat
-mode = 0 #0=play,1=sound,2=sequence,3=layer
+# PROGRAM VARIABLES
+sequence1 = [1, 0, 0, 1, 0, 1, 0, 0]  # kick
+sequence2 = [0, 0, 1, 0, 0, 0, 1, 0]  # snare
+sequence3 = [1, 1, 0, 1, 1, 1, 0, 1]  # hihat
+mode = 0  # 0=play,1=sound,2=sequence,3=layer
 play = True
 btn1_debounce = True
 i = 0
 
 audio.play(mixer)
 
+
 def wait(time):
     deadline = ticks_add(ticks_ms(), time)
     while ticks_less(ticks_ms(), deadline):
         pass
-def play_sound(mixer_voice,ssound):
+
+
+def play_sound(mixer_voice, ssound):
     mixer.voice[mixer_voice].play(ssound)
-def set_led(index,vvalue):
+
+
+def set_led(index, vvalue):
     leds[index].value = vvalue
 
-def play_sound_and_light(ss,i):
+
+def play_sound_and_light(ss, i):
     mixer.voice[i].play(ss)
     leds[i].value = True
     deadline = ticks_add(ticks_ms(), bpm_millis)
     while ticks_less(ticks_ms(), deadline):
         pass
     leds[i].value = False
+
 
 def mapp(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
@@ -97,49 +126,121 @@ def mapp(value, leftMin, leftMax, rightMin, rightMax):
 
     # Convert the 0-1 range into a value in the right range.
     return rightMin + (valueScaled * rightSpan)
+
+
 j = 0
-while True:
-    if (j>3):
-        j=0
-    set_led(j,True)
-    if play==True:
-        if (sequence1[j]==1):
-            play_sound(0,kick)
-        if (sequence2[j]==1):
-            play_sound(2,snare)
-        if (sequence3[j]==1):
-            play_sound(2,hihat)
-    start_timer = ticks_ms() #start timer
-    #DO STUFF HERE
-    #bisogna sperare che il codice qua in mezzo esegua in meno tempo di un battito
-    pot1_value = mapp(pot1.value,65535,0,0,65535)
-    bpm = mapp(pot1_value,0,65535,40,300)
+# while True:
+#     if (j>3):
+#         j=0
+#     set_led(j,True)
+#     if play==True:
+#         if (sequence1[j]==1):
+#             play_sound(0,kick)
+#         if (sequence2[j]==1):
+#             play_sound(2,snare)
+#         if (sequence3[j]==1):
+#             play_sound(2,hihat)
+#     start_timer = ticks_ms() #start timer
+#     DO STUFF HERE
+#     bisogna sperare che il codice qua in mezzo esegua in meno tempo di un battito
+#     pot1_value = mapp(pot1.value,65535,0,0,65535)
+#     bpm = mapp(pot1_value,0,65535,40,300)
+#     if pot1_value < 13107:
+#         mode = 0 #play
+#     elif pot1_value < 26214:
+#         mode = 1 #sound
+#     elif pot1_value < 39321:
+#         mode = 2 #pattern
+#     elif pot1_value < 52428:
+#         mode = 3
+#     else:
+#         mode = 4
+
+#     if btn1.value == False and btn1_debounce==True:
+#         btn1_debounce = False
+#         check mode and answer correctly
+#         print(pot1_value)
+
+#         if mode == 0:
+#             play = not play
+#         if mode == 1:
+#             sequence1[0] = not sequence1[0]
+#     if btn1.value == True:
+#         btn1_debounce = True
+
+
+#     FINISH TO DO STUFF HERE
+#     wait(bpm_millis)
+#     set_led(j,False)
+#     j+=1
+#     start_timer = 0
+
+
+async def play_sound_async(delay, mixer_voice, ssound):
+    while True:
+        mixer.voice[mixer_voice].play(ssound)
+        await asyncio.sleep(
+            delay
+        )  # ogni quanto la funzione viene eseguita, lo "sleep" fa fare yeald e rilascia la cpu
+
+
+async def play_sound_and_light_async(delay, sound, mixer_voice, sequence):
+    a = 0
+    while play:
+        if a > 7:
+            a = 0
+        if a == 0:
+            leds[7].value = False
+        if a != 0:
+            leds[a - 1].value = False
+        if sequence[a] == 1:
+            mixer.voice[mixer_voice].play(sound)
+            leds[a].value = True
+        a += 1
+        await asyncio.sleep(delay)  # yeald
+
+
+def set_led_async(index, vvalue):
+    leds[index].value = vvalue
+
+
+async def update_pot_value_async(delay):
+    pot1_value = mapp(pot1.value, 65535, 0, 0, 65535)
     if pot1_value < 13107:
-        mode = 0 #play
+        mode = 0  # play
     elif pot1_value < 26214:
-        mode = 1 #sound
+        mode = 1  # sound
     elif pot1_value < 39321:
-        mode = 2 #pattern
+        mode = 2  # pattern
     elif pot1_value < 52428:
         mode = 3
     else:
         mode = 4
+    await asyncio.sleep(delay)
 
-    if btn1.value == False and btn1_debounce==True:
-        btn1_debounce = False
-        #check mode and answer correctly
-        print(pot1_value)
 
+async def btn1_async(delay):  # TODO: check if it works
+    prev_value = current_btn_value
+    current_btn_value = btn1.value
+    if current_btn_value == 0 and current_btn_value != prev_value:  # button is pressed
+        current_btn_value = 1
         if mode == 0:
             play = not play
-        if mode == 1:
+        elif mode == 1:
             sequence1[0] = not sequence1[0]
-    if btn1.value == True:
-        btn1_debounce = True
+    await asyncio.sleep(delay)
 
-    #
-    #FINISH TO DO STUFF HERE
-    wait(bpm_millis)
-    set_led(j,False)
-    j+=1
-    start_timer = 0
+
+async def main():
+    asyncio.create_task(play_sound_and_light_async(bpm_float, kick, 0, sequence1))
+    asyncio.create_task(play_sound_and_light_async(bpm_float, snare, 1, sequence2))
+    asyncio.create_task(play_sound_and_light_async(bpm_float, hihat, 2, sequence3))
+
+    asyncio.create_task(update_pot_value_async(0.4))
+
+    while True:
+        print("done")
+        await asyncio.sleep(1)
+
+
+asyncio.run(main())
